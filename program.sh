12a2f8581b6a04f3f922b9627d81fc228c0ffccb
dgraph-scout <dgraph-scout@program>
2021-08-01 04:02:34 +0300

    from program.sh i remove  no-history push

--- program.sh
+++ program.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am dgraph-scout program"
-  git push -f -u origin main
+repl(){
+  clj -A:repl
 }
 
 "$@"
